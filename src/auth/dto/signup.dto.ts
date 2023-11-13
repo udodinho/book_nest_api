aac98177dd51514dbdfa71e4a5bd2d2f19ae1465
udodinho <udodinho200@gmail.com>
2023-11-13 15:22:25 +0100

    Create signup dto

---
+++ src/auth/dto/signup.dto.ts
@@ -0,0 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
+
+export class SignUpDto {
+    @IsNotEmpty()
+    @IsString()
+    readonly name: string;
+
+    @IsNotEmpty()
+    @IsEmail({}, { message: "Please enter valid email"})
+    readonly email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(6)
+    readonly password: string;
+}
